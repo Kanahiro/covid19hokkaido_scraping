f808fd1f3e71d53e1245d5c5163b5319051b4c21
Kanahiro <mediterranean1769@gmail.com>
2020-03-11 11:56:58 +0900

    WIP

--- main.py
+++ main.py
@@ -4,30 +4,36 @@ import datetime
 import glob
 import os
 import urllib.request
+
 from patients import PatientsReader
+from . import settings
 
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+REMOTE_SOURCES = settings.REMOTE_SOURCES
 
 class CovidDataManager:
     def __init__(self):
         self.data = {
-            'contacts':{},
-            'querents':{},
-            'patients':{},
-            'patients_summary':{},
-            'discharges':{},
-            'discharges_summary':{},
-            'inspections':{},
-            'inspections_summary':{},
-            'better_patients_summary':{},
             'last_update':datetime.datetime.now(JST).isoformat(),
-            'main_summary':{}
         }
 
     def fetch_data(self):
         pr = PatientsReader()
         self.data['patients'] = pr.make_patients_dict()
         self.data['patients_summary'] = pr.make_patients_summary_dict()
+
+    def fetch_datas(self):
+        for key in REMOTE_SOURCES:
+            self.fetch_data_of(key)
+
+    def fetch_data_of(self, key):
+        datatype = REMOTE_SOURCES[key]['type']
+        data = {}
+        if datatype == 'odp':
+            
+        elif datatype == 'csv':
+        
+        self[key] = data
 
     def export_csv(self):
         for key in self.data:
@@ -87,44 +93,76 @@ class CovidDataManager:
                 'last_update':last_modified_time
             }
 
-    def import_csv_from_odp(self):
-        responce = urllib.request.urlopen('https://www.harp.lg.jp/opendata/api/package_show?id=752c577e-0cbe-46e0-bebd-eb47b71b38bf')
-        print(responce.getcode())
-        if responce.getcode() == 200:
-            loaded_json = json.loads(responce.read().decode('utf-8'))
-            if loaded_json['success'] == True:
-                resources = loaded_json['result']['resources']
-                for resource in resources:
-                    url = resource['download_url']
-                    request_file = urllib.request.urlopen(url)
-                    if request_file.getcode() == 200:
-                        f = request_file.read().decode('utf-8')
-                        filename = resource['filename'].rstrip('.csv')
-                        last_modified_time = resource['updated']
-                        datas = []
-                        rows = [row for row in csv.reader(f.splitlines())]
-                        header = rows[0]
-                        maindatas = rows[1:]
-                        for d in maindatas:
-                            data = {}
-                            for i in range(len(header)):
-                                if filename == "current_patients":
-                                    if i <= 1:
-                                        if header[i] == '患者数':
-                                            data['subtotal'] = int(d[i])
-                                        if header[i] == '日付':
-                                            data['date'] = d[i]
-                                else:
-                                    if header[i] == '小計':
-                                        data['subtotal'] = int(d[i])
-                                    if header[i] == '日付':
-                                        data['date'] = d[i]
-                            datas.append(data)
+    def import_csv_from_odp(self, odp_url='https://www.harp.lg.jp/opendata/api/package_show?id=752c577e-0cbe-46e0-bebd-eb47b71b38bf'):
+        responce = urllib.request.urlopen(odp_url)
 
-                        self.data[filename] = {
-                            'data':datas,
-                            'last_update':last_modified_time
-                        }
+        status_code = responce.getcode()
+        if not status_code == 200:
+            print(status_code, 'error occured')
+            return
+
+        loaded_json = json.loads(responce.read().decode('utf-8'))
+        if not loaded_json['success']:
+            print('get json error')
+            return
+
+        resources = loaded_json['result']['resources']
+        for resource in resources:
+            url = resource['download_url']
+            request_file = urllib.request.urlopen(url)
+            if request_file.getcode() == 200:
+                f = request_file.read().decode('utf-8')
+                filename = resource['filename'].rstrip('.csv')
+                last_modified_time = resource['updated']
+                datas = []
+                rows = [row for row in csv.reader(f.splitlines())]
+                header = rows[0]
+                maindatas = rows[1:]
+                for d in maindatas:
+                    data = {}
+                    for i in range(len(header)):
+                        if filename == "current_patients":
+                            if i <= 1:
+                                if header[i] == '患者数':
+                                    data['subtotal'] = int(d[i])
+                                if header[i] == '日付':
+                                    data['date'] = d[i]
+                        else:
+                            if header[i] == '小計':
+                                data['subtotal'] = int(d[i])
+                            if header[i] == '日付':
+                                data['date'] = d[i]
+                    datas.append(data)
+
+                self.data[filename] = {
+                    'data':datas,
+                    'last_update':last_modified_time
+                }
+
+    def import_csv_from(self, url):
+        request_file = urllib.request.urlopen(url)
+        if not request_file.getcode() == 200:
+            return
+        
+        f = request_file.read().decode('utf-8')
+        filename = os.path.splitext(os.path.basename(csvfile))[0]
+        datas = []
+        rows = [row for row in csv.reader(f.splitlines())]
+        header = rows[0]
+        maindatas = rows[1:]
+        for d in maindatas:
+            data = {}
+            for i in range(len(header)):
+                if header[i] == '小計':
+                    data['subtotal'] = int(d[i])
+                if header[i] == '日付':
+                    data['date'] = d[i]
+            datas.append(data)
+
+        self.data[filename] = {
+            'data': datas,
+            'last_update': datetime.datetime.now(JST).isoformat()
+        }
 
     def import_csv_from_sdp_contacts(self):
         request_file = urllib.request.urlopen('https://ckan.pf-sapporo.jp/dataset/f6338cc2-dd6b-43b6-98a3-cd80b05b6a36/resource/e9e6f062-cafd-4aea-992f-039e2e26f4ac/download/contacts.csv')
